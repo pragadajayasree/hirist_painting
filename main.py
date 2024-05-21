a078456cfb154bb23c64e715d2e7288bc66beead
pragadajayasree <134396518+pragadajayasree@users.noreply.github.com>
2024-05-21 19:41:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
-"""import colorgram
-def co(colors):
-    colours = []
-    for i in range(len(colors)):
-     rgb=colors[i].rgb
-     p=(rgb.r,rgb.g,rgb.b)
-     colours.append(p)
-    return colours
-
-colors = colorgram.extract('image.jpg',10)
-print(co(colors))"""
 import turtle as t
 import random
-tt= t.Turtle()
+
+tt = t.Turtle()
 tt.speed(0)
 t.colormode(255)
 color = [(215, 155, 114), (135, 170, 191), (212, 130, 147), (219, 75, 93), (31, 116, 147), (236, 209, 104)]
@@ -21,21 +11,18 @@ tt.penup()
 tt.forward(320)
 tt.setheading(0)
 for i in range(10):
-  for j in range(10):
-    tt.dot(20,random.choice(color))
+    for j in range(10):
+        tt.dot(20, random.choice(color))
+        tt.penup()
+        tt.forward(50)
+        tt.pendown()
+    tt.setheading(90)
     tt.penup()
     tt.forward(50)
-    tt.pendown()
-  tt.setheading(90)
-  tt.penup()
-  tt.forward(50)
-  tt.setheading(180)
-  tt.penup()
-  tt.forward(500)
-  tt.setheading(0)
+    tt.setheading(180)
+    tt.penup()
+    tt.forward(500)
+    tt.setheading(0)
 
-
-
-
-screen=t.Screen()
+screen = t.Screen()
 screen.exitonclick()
